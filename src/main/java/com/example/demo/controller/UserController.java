8636c7d122e9e8ca5a17b694a8fe00dae56ec1e4
Nafiul Islam <nafiul.islam@synesisit.info>
2022-08-06 02:50:08 +0600

    use put,delete,post api using dto conversion

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,21 +1,45 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.UserDto;
+import com.example.demo.model.User;
 import com.example.demo.service.userService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@EnableAutoConfiguration
 public class UserController {
     @Autowired
     userService userService;
+    @Autowired
+     ModelMapper modelMapper;
     @GetMapping("/userList")
     public List<UserDto> getAllUserList(){
         System.out.println("call this api");
         return userService.getAllUserList();
+    }
+    @PostMapping("/saveUser")
+    public User saveUser(@RequestBody UserDto userDto){
+
+        User user = modelMapper.map(userDto,User.class);
+
+        return userService.saveUser(user);
+
+    }
+    @PutMapping("/editUser/{email}")
+    public User editUser(@PathVariable("email") String email, @RequestBody UserDto userDto){
+        User user1 = modelMapper.map(userDto,User.class);
+        return userService.editUser(email,user1);
+
+    }
+    @DeleteMapping("/deleteUser/{email}")
+    public void deleteUser(@PathVariable("email") String email){
+
+       userService.deleteUser(email);
     }
 
 }
--- src/main/java/com/example/demo/service/userService.java
+++ src/main/java/com/example/demo/service/userService.java
@@ -4,13 +4,17 @@ import com.example.demo.dto.UserDto;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.userRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 @Service
-public class userService {
+public class userService  {
+    @Autowired
     userRepository userRepository ;
+
+
 
     private userService(com.example.demo.repository.userRepository userRepository) {
         this.userRepository = userRepository;
@@ -18,12 +22,34 @@ public class userService {
     public List<UserDto> getAllUserList(){
         return userRepository.findAll().stream().map(this::convertEntityToDto).collect(Collectors.toList());
     }
+    public User saveUser(User user){
 
+        return userRepository.save(user);
+    }
+    public void deleteUser(String email){
+         userRepository.delete(userRepository.getUserByEmail(email));
+    }
+    public User editUser(String email,User user){
+        User user1 = userRepository.getUserByEmail(email);
+        user1.setFirstName(user.getFirstName());
+        user1.setLastName(user.getLastName());
+        user1.setEmail(user.getEmail());
+        return userRepository.save(user1);
+    }
+    //entity to dto
     private UserDto convertEntityToDto(User user){
         UserDto userDto = new UserDto();
         userDto.setUserFirstName(user.getFirstName());
         userDto.setUserLastName(user.getLastName());
         userDto.setUserEmail(user.getEmail());
         return userDto;
+    }
+    //convert DTO to entity
+    private User convertDTOtoEntity(UserDto userDto){
+        User user = new User();
+        user.setFirstName(userDto.getUserFirstName());
+        user.setLastName(userDto.getUserLastName());
+        user.setEmail(userDto.getUserEmail());
+        return user;
     }
 }
